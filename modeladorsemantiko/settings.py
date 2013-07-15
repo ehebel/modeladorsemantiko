efe1ef89af75cef1f5ed176ae985de0404953d6a
Esteban Hebel <ehebel@gmail.com>
2013-07-15 00:21:33 -0400

    Sistema en funcionamiento

--- modeladorsemantiko/settings.py
+++ modeladorsemantiko/settings.py
@@ -20,8 +20,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',
-        #'NAME': os.path.join(PROJECT_PATH, 'db.sqlite'), # Or path to database file if using sqlite3.
+        #'NAME': 'db.sqlite',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'), # Or path to database file if using sqlite3.
         #'NAME': 'semantik_modelador',
         # The following settings are not used with sqlite3:
         'USER':'',

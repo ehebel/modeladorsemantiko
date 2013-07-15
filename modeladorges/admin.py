89d139d79c7fab958a09e39dedb3418afa1d3094
Esteban Hebel <ehebel@gmail.com>
2013-07-15 17:54:51 -0400

    Sistema usando Subiendo modelos y tablas

--- modeladorges/admin.py
+++ modeladorges/admin.py
@@ -4,7 +4,7 @@ autocomplete_light.autodiscover()
 from django.contrib import admin
 admin.autodiscover()
 
-from modeladorges.models import ciediez,casdiagnostico,casprocedimiento,ges_patologia
+from modeladorges.models import *
 
 
 class GesAdmin(admin.ModelAdmin):
@@ -15,6 +15,8 @@ admin.site.register(ges_patologia, GesAdmin)
 admin.site.register(ciediez)
 admin.site.register(casprocedimiento)
 admin.site.register(casdiagnostico)
+admin.site.register(concepto)
+admin.site.register(descripcione)
 
 
 __author__ = 'ehebel'

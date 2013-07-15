a6a0d79fc74aad5b07f1823a09579f6b1ded0076
Esteban Hebel <ehebel@gmail.com>
2013-07-15 12:48:50 -0400

    Sistema usando SQLite3

--- modeladorsemantiko/settings.py
+++ modeladorsemantiko/settings.py
@@ -28,10 +28,9 @@ DATABASES = {
 #        'PORT': '',                      # Set to empty string for default.
         #PRUEBAS:
         #'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'), # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'), # Or path to database file if using sqlite3.
         'USER':'',
         'PASSWORD':'',
         'HOST': '',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.

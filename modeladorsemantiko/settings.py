67f9343843c25b6899925ec5d0e63fff9b643b18
Esteban Hebel <ehebel@gmail.com>
2013-07-15 09:42:07 -0400

    Sistema en funcionamiento

--- modeladorsemantiko/settings.py
+++ modeladorsemantiko/settings.py
@@ -82,7 +82,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home1/semantik/public_html/modelador/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"

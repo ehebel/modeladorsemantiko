08ce51a76c97fe9d205c25fe5aea0afc990ecf34
Esteban Hebel <ehebel@gmail.com>
2013-09-02 09:45:13 -0400

    Se agrega app modeladorFarmacos v1.00.6 con mejoras a los admin incluyendo requerimientos

--- modeladorsemantiko/settings.py
+++ modeladorsemantiko/settings.py
@@ -27,7 +27,7 @@ DATABASES = {
         'HOST': 'localhost',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
         #PRUEBAS:
-        #TODO Cambiar Conexión de BD a MySQL
+        #TODO Cambiar Conexión de BD a MySQL NO OLVIDAR
 #        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
 #        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite'), # Or path to database file if using sqlite3.
 #        # The following settings are not used with sqlite3:

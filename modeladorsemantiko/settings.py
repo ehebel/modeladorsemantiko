9c00c6b9c3da9c96f815cb69f698e34710f57a46
Esteban Hebel <ehebel@gmail.com>
2013-12-17 22:12:53 -0300

    Agrego modeloFarmacos2 y bajo temporalmente modeloFarmacos

--- modeladorsemantiko/settings.py
+++ modeladorsemantiko/settings.py
@@ -134,7 +134,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -218,7 +218,7 @@ LOGGING = {
         }
 }
 
-INTERNAL_IPS = ('127.0.0.1',)
+#INTERNAL_IPS = ('127.0.0.1',)
 
 try:
     from modeladorsemantiko.local_settings import *
